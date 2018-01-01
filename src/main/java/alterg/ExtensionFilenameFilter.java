d127447ddb7fb546f4c1a1b6d7b4ee699a5f6a91
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-01 18:39:05 +0300

    add extension filter for reading files

---
+++ src/main/java/alterg/ExtensionFilenameFilter.java
@@ -0,0 +1,17 @@
+package alterg;
+
+import lombok.AllArgsConstructor;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+@AllArgsConstructor
+public class ExtensionFilenameFilter implements FilenameFilter {
+
+    private final String extension;
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.toLowerCase().endsWith(extension);
+    }
+}
