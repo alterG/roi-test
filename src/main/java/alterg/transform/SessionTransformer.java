55dfcfbfce9a75543d2503368a6ef88ffc2b1f40
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-02 14:06:48 +0300

    fix transformer (data set)

--- src/main/java/alterg/transform/SessionTransformer.java
+++ src/main/java/alterg/transform/SessionTransformer.java
@@ -3,13 +3,14 @@ package alterg.transform;
 import alterg.bean.SessionDataBean;
 import alterg.entity.SessionData;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public class SessionTransformer implements Transformer<SessionDataBean, SessionData> {
 
     @Override
     public SessionData transform(SessionDataBean source) {
         SessionData result = new SessionData();
-        result.setTime(new DateTime(source.getTimeStamp()));
+        result.setStartTime(new DateTime(source.getTimeStamp() * 1000L, DateTimeZone.UTC));
         result.setUserId(parseId(source.getUserId()));
         result.setUrl(source.getUrl());
         result.setWastedTime(source.getWastedTime());
@@ -20,6 +21,6 @@ public class SessionTransformer implements Transformer<SessionDataBean, SessionD
      * method works with "userXXX", where XXX is required id
      */
     private int parseId(String userId) {
-        return Integer.parseInt(userId.substring(userId.lastIndexOf("user")));
+        return Integer.parseInt(userId.substring(userId.lastIndexOf("user") + "user".length()));
     }
 }
