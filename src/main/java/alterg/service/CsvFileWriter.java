b5f1460c559c5b0a67074098806be302f76f19d7
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-05 00:34:30 +0300

    refactor CsvFileWriter
    * throw exception from method (we can't handle it)
    * change access modifier
    * add javadoc

--- src/main/java/alterg/service/CsvFileWriter.java
+++ src/main/java/alterg/service/CsvFileWriter.java
@@ -8,19 +8,23 @@ import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+/**
+ * Class for writing data (@see SessionDataOutputBean) to csv files
+ */
 public class CsvFileWriter {
 
-    public static final CellProcessor[] PROCESSORS = new CellProcessor[]{new NotNull(), new NotNull(), new NotNull()};
-    public static final String[] HEADERS = {"userId", "url", "averageTimeVisit"};
+    private static final CellProcessor[] PROCESSORS = new CellProcessor[]{new NotNull(), new NotNull(), new NotNull()};
+    private static final String[] HEADERS = {"userId", "url", "averageTimeVisit"};
 
-    public void write(Map<LocalDate, List<SessionDataOutputBean>> sessionDataMap, String filename) throws IOException {
-        try (FileWriter writer = new FileWriter(filename);
+    public void write(Map<LocalDate, List<SessionDataOutputBean>> sessionDataMap, File file) throws IOException {
+        try (FileWriter writer = new FileWriter(file);
              ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, CsvPreference.STANDARD_PREFERENCE)) {
             for (LocalDate date : sessionDataMap.keySet()) {
                 csvWriter.writeHeader(date.toString("dd-MMM-yyyy", Locale.ENGLISH));
