95dc8cba58400251cdef08ab9ad7a8575bcf0722
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-05 00:17:35 +0300

    refactor CsvFileReader
    * throw exception from method (we can't handle it here)
    * change access modifier (constants shouldn't be used out of class)
    * add javadoc

--- src/main/java/alterg/service/CsvFileReader.java
+++ src/main/java/alterg/service/CsvFileReader.java
@@ -8,27 +8,28 @@ import org.supercsv.io.CsvBeanReader;
 import org.supercsv.io.ICsvBeanReader;
 import org.supercsv.prefs.CsvPreference;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Class for read csv data to beans (@see SessionDataInputBean)
+ */
 public class CsvFileReader {
 
-    public static final String[] HEADERS = {"timeStamp", "userId", "url", "wastedTime"};
-    public static final CellProcessor[] PROCESSORS = new CellProcessor[]{
+    private static final String[] HEADERS = {"timeStamp", "userId", "url", "wastedTime"};
+    private static final CellProcessor[] PROCESSORS = new CellProcessor[]{
         new NotNull(new ParseInt()), new NotNull(), new NotNull(), new NotNull(new ParseInt())};
 
-    public List<SessionDataInputBean> read(String fileName) {
+    public List<SessionDataInputBean> read(File file) throws IOException {
         List<SessionDataInputBean> inputBeans = new ArrayList<>();
-        try (ICsvBeanReader reader = new CsvBeanReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE)) {
+        try (ICsvBeanReader reader = new CsvBeanReader(new FileReader(file), CsvPreference.STANDARD_PREFERENCE)) {
             SessionDataInputBean bean;
             while ((bean = reader.read(SessionDataInputBean.class, HEADERS, PROCESSORS)) != null) {
                 inputBeans.add(bean);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return inputBeans;
     }
