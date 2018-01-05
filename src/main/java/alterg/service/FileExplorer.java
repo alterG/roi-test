844630cf48b7091ef3787e384c9f6611f1920a87
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-05 21:58:10 +0300

    add support for other explorer commands

--- src/main/java/alterg/service/FileExplorer.java
+++ src/main/java/alterg/service/FileExplorer.java
@@ -1,5 +1,7 @@
 package alterg.service;
 
+import lombok.RequiredArgsConstructor;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,56 +10,102 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@RequiredArgsConstructor
 public class FileExplorer {
 
+    private final Pattern commandPattern = Pattern.compile("^\\s*([a-zA-Z]+)");
+    private final Pattern filePattern = Pattern.compile("\\s+(\\w+)");
+    private final SessionDataProcessor processor;
     private File pwd;
-    private final Pattern firstArgument = Pattern.compile("^\\s*([a-zA-Z]+)");
-    private final Pattern secondArgument = Pattern.compile("\\s+([._\\w]+)");
 
     public void runExplorer() {
-        pwd = new File("").getAbsoluteFile();
+        pwd = processor.getInputDirectory();
         Scanner scanner = new Scanner(System.in);
-        while (pwd != null) {
+        printMenu();
+
+        while (true) {
             System.out.print(pwd.getAbsolutePath() + "@user: ");
-            String userInput = scanner.next();
-            Matcher matcher = firstArgument.matcher(userInput);
+            String userInput = scanner.nextLine();
+            Matcher matcher = commandPattern.matcher(userInput);
             if (!matcher.find()) {
                 System.out.println("Unknown command.");
                 continue;
             }
-            String command = matcher.group(1);
-            File commandArgument = getSecondArgument(userInput.substring(matcher.end(1)))
-            switch (command) {
-                case "ls":
-                    ls(commandArgument);
-                    break;
-                case "mov":
-//                    File commandArgument2 = getSecondArgument()
-                    mov(commandArgument);
-                    break;
-                case "menu":
-                    printMenu();
-                    break;
-                case "rm":
-                    rm(commandArgument);
-                    break;
-                case "exit":
-                    System.exit(0);
-                default:
+            String stringCommand = matcher.group(1);
+            Commands command = Commands.parse(stringCommand);
+            String userInputWithoutCommand = userInput.substring(matcher.end(1));
+            try {
+                executeCommand(command, userInputWithoutCommand);
+            } catch (Exception e) {
+                e.getMessage();
             }
+        }
+    }
+
+    /**
+     * Execute command
+     */
+    private void executeCommand(Commands command, String userInput) {
+        switch (command) {
+            case CALC:
+                System.out.println("Processing session info...");
+                processor.process();
+                System.out.println("Session info is calculated.");
+                break;
+            case LS:
+                ls(userInput);
+                break;
+            case CD:
+                try {
+                    cd(userInput);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Can't change working directory.");
+                }
+                break;
+            case CAT:
+                try {
+                    cat(userInput);
+                } catch (IOException e) {
+                    System.out.println("File can't be printed");
+                }
+                break;
+            case MKDIR:
+                if (!mkdir(userInput)) {
+                    System.out.println("Can't create directory.");
+                }
+                break;
+            case MOV:
+                try {
+                    mov(userInput);
+                } catch (Exception e) {
+                    e.getMessage();
+                }
+                break;
+            case MENU:
+                printMenu();
+                break;
+            case RM:
+                if (rm(userInput)) {
+                    System.out.println("File was deleted.");
+                } else {
+                    System.out.println("File can't be deleted.");
+                }
+                break;
+            case EXIT:
+                exit();
+                break;
+            default:
+                System.out.println("Unknown command.");
         }
     }
 
     /**
      * parse second argument from user input
      * note: ".." means parent file
-     * @param source
-     * @param isExistValidation
-     * @return
-     * @throws IllegalArgumentException
      */
-    private File getSecondArgument(String source, boolean isExistValidation) throws IllegalArgumentException {
-        Matcher matcher = secondArgument.matcher(source);
+    private File getSecondArgument(String source, Commands command) throws IllegalArgumentException {
+
+        Matcher matcher = filePattern.matcher(source);
         if (!matcher.find()) {
             throw new IllegalArgumentException("Wrong argument.");
         }
@@ -78,26 +126,41 @@ public class FileExplorer {
         return parsedFile;
     }
 
+    private boolean isStringEmpty(String source) {
+        return source.matches("\\s*");
+    }
+
+    private boolean isParentSymbol(String source) {
+        return source.matches("\\s*\\.\\.");
+    }
 
     /**
      * Change pwd to given directory
-     *
-     * @param file target directory
      */
-    private void cd(File file) throws IllegalArgumentException {
-        if (!file.canRead() || !file.canExecute()) {
+    private void cd(String source) throws IllegalArgumentException {
+        File argument = null;
+        if (isParentSymbol(source)) {
+            argument = pwd.getParentFile();
+        } else {
+            Matcher matcher = filePattern.matcher(source);
+            String fileName = matcher.group(1);
+            argument = new File(pwd, fileName);
+        }
+        if (!argument.canRead() || !argument.canExecute()) {
             throw new IllegalArgumentException("Access denied.");
         }
-        pwd = file;
+        pwd = argument;
     }
 
     /**
      * Print content of file
      *
-     * @param file to print it's content
      * @throws IOException with reason of it
      */
-    private void cat(File file) throws IOException {
+    private void cat(String source) throws IOException {
+        Matcher matcher = filePattern.matcher(source);
+        String argument = matcher.group(1);
+        File file = new File(pwd, argument);
         try {
             System.out.println(new String(Files.readAllBytes(file.toPath())));
         } catch (IOException e) {
@@ -115,40 +178,66 @@ public class FileExplorer {
 
     /**
      * Create directory
+     * @return true if operation is successful
      */
-    private void mkdir(File directory) {
-        directory.mkdir();
+    private boolean mkdir(String source) {
+        Matcher matcher = filePattern.matcher(source);
+        String argument = matcher.group(1);
+        File directory = new File(pwd, argument);
+        return directory.mkdir();
     }
 
     /**
      * Delete file or directory
      *
-     * @param file to delete
      * @return true if file has been deleted
      */
-    private void rm(File file) {
-        if (file.delete()) {
-            System.out.println(file.getName() + "was deleted.");
-        } else {
-            System.out.println("can't delete " + file.getName());
-        }
+    private boolean rm(String source) {
+        Matcher matcher = filePattern.matcher(source);
+        String argument = matcher.group(1);
+        File file = new File(pwd, argument);
+        return file.delete();
     }
 
     /**
      * Print files in directory
-     *
-     * @param file target directory
+     * @param source user input without command part
      */
-    private void ls(File file) {
-        for (File x : file.listFiles()) {
-            System.out.println(String.format("%s\t%s", x.isFile() ? "-" : "d", x.getName()));
+    private void ls(String source) {
+        File argument = null;
+        if (isStringEmpty(source)) {
+            argument = pwd;
+        }
+        if (isParentSymbol(source)) {
+            argument = pwd.getParentFile();
+        }
+        Matcher matcher = filePattern.matcher(source);
+        String fileName = matcher.group(1);
+        argument = new File(pwd, fileName);
+        for (File file : argument.listFiles()) {
+            System.out.println(String.format("%s\t%s", file.isFile() ? "-" : "d", file.getName()));
         }
     }
 
-    private void mov(File file, File directory) {
-        file.renameTo(new File(directory, file.getName()));
+    /**
+     * move file to directory
+     */
+    private void mov(String source) {
+        Matcher matcher = filePattern.matcher(source);
+        String fileName = matcher.group(1);
+        File argumentFile = new File(pwd, fileName);
+        Matcher matcher2 = filePattern.matcher(source.substring(matcher.end(1)));
+        String directoryName = matcher2.group(1);
+        File argumentDirectory = new File(pwd, directoryName);
+        if (!argumentDirectory.isDirectory()) {
+            throw new IllegalArgumentException(argumentDirectory + " isn't directory.");
+        }
+        argumentFile.renameTo(new File(argumentDirectory, argumentFile.getName()));
     }
 
+    /**
+     * show menu
+     */
     private void printMenu() {
         System.out.println("Command list:\n"
                            + "* calc - to calculate sessions average visit time\n"
@@ -161,17 +250,12 @@ public class FileExplorer {
                            + "* exit - close program");
     }
 
+    /**
+     * close program
+     */
     private void exit() {
         System.out.println("Program is closing...");
         System.exit(0);
-    }
-
-    private void printCalcStart() {
-        System.out.println("Processing session info...");
-    }
-
-    private void printCalcEnd() {
-        System.out.println("Session info is calculated...");
     }
 
 
