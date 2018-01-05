a5b4149b5cad3291aeea8d465f928b5a9424eedd
Igor_Shchipanov <Igor_Shchipanov@epam.com>
2018-01-05 19:33:31 +0300

    refactor

--- src/main/java/alterg/service/ProcessorWorker.java
+++ src/main/java/alterg/service/ProcessorWorker.java
@@ -3,7 +3,6 @@ package alterg.service;
 import alterg.dto.SessionAverageTimeBean;
 import alterg.dto.SessionData;
 import alterg.dto.SessionDataInputBean;
-import alterg.transform.SessionAverageTimeBeanTransformer;
 import alterg.transform.SessionTransformer;
 import lombok.AllArgsConstructor;
 import org.joda.time.LocalDate;
@@ -15,22 +14,20 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
-public class AverageCsvProcessingTask implements Runnable {
+public class ProcessorWorker implements Runnable {
 
-    public static final String PREFIX_RESULT_FILE = "avg_";
     private File inputFile;
     private File outputDirectory;
-    private SessionTransformer transformer;
 
     @Override
     public void run() {
         try {
-        CsvFileReader reader = new CsvFileReader();
-        List<SessionDataInputBean> inputBeans = reader.read(inputFile);
-        List<SessionData> sessionDataList = transform(inputBeans);
-        Map<LocalDate, List<SessionAverageTimeBean>> sessionOutputBeans = process(sessionDataList);
-        CsvFileWriter writer = new CsvFileWriter();
-        File outputFile = getOutputFile(inputFile, outputDirectory);
+            CsvFileReader reader = new CsvFileReader();
+            List<SessionDataInputBean> inputBeans = reader.read(inputFile);
+            List<SessionData> sessionDataList = transform(inputBeans);
+            Map<LocalDate, List<SessionAverageTimeBean>> sessionOutputBeans = process(sessionDataList);
+            CsvFileWriter writer = new CsvFileWriter();
+            File outputFile = getOutputFile(inputFile, outputDirectory);
             writer.write(sessionOutputBeans, outputFile);
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,17 +35,18 @@ public class AverageCsvProcessingTask implements Runnable {
     }
 
     private Map<LocalDate, List<SessionAverageTimeBean>> process(List<SessionData> sessionDataList) {
-        SessionDataHandler handler = new SessionDataHandler(new SessionAverageTimeBeanTransformer(), sessionDataList);
+        SessionDataHandler handler = new SessionDataHandler(sessionDataList);
         handler.divideSessionsByDay();
         return handler.getSessionOutputBeans();
     }
 
     private File getOutputFile(File inputFile, File outputDirectory) {
-        String outputFileName = PREFIX_RESULT_FILE + inputFile.getName();
+        String outputFileName = SessionDataProcessor.PREFIX_RESULT_FILE + inputFile.getName();
         return new File(outputDirectory, outputFileName);
     }
 
     private List<SessionData> transform(List<SessionDataInputBean> inputBeans) {
+        SessionTransformer transformer = new SessionTransformer();
         return inputBeans.stream()
             .map(x -> transformer.transform(x))
             .collect(Collectors.toList());
